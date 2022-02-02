5c2d1dd190fb238d5a598f938b4ff707d1c371dc
stamander <iida.atsushi.fh@gmail.com>
2022-02-02 21:47:44 +0900

    Create 0202.scala

---
+++ 0202.scala
@@ -0,0 +1,15 @@
+object Main{
+  def main (args:Array[String]):Unit ={
+
+    val num1 = 5
+    val num2 = 1
+
+    val sum = num1*num2
+
+    println(sum)
+
+    if(sum < 10){
+      println("nice!")
+    }
+  }
+}
