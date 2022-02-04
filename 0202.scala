f74f976b80dbac9ebd94e54019f29433d806a1eb
stamander <iida.atsushi.fh@gmail.com>
2022-02-04 09:33:24 +0900

    Update 0202.scala

--- 0202.scala
+++ 0202.scala
@@ -1,15 +1,9 @@
+import scala.io.StdIn.{readLine, readInt}
+
 object Main{
   def main (args:Array[String]):Unit ={
+    var s = readLine("input> ")
+    print(s)
 
-    val num1 = 5
-    val num2 = 1
-
-    val sum = num1*num2
-
-    println(sum)
-
-    if(sum < 10){
-      println("nice!")
-    }
   }
 }
