e87766dd9b69f50a689579b9fa92f30d7ae1d4b4
stamander <iida.atsushi.fh@gmail.com>
2022-02-25 09:11:05 +0900

    型の渡しはいらない
    引数下で型を渡してしまうとエラーが出る

---
+++ src/function/parameter.ts
@@ -0,0 +1,11 @@
+//オプションパラメータ
+export const isUserSignedIn = (userId: string, username?: string): boolean => {
+  if (userId === 'ABC') {
+    console.log('Function parameters sample 1: User is signed in! Username is', username)
+    return true
+  } else {
+    console.log('Function parameters sample 2: User is not signed in')
+    return false
+  }
+}
+
--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,17 @@
 
 
 //基本の型定義
-import { anySample, noExistSample, primitiveSample, unknownSample } from "./basic";
-anySample();
-noExistSample();
-primitiveSample();
-unknownSample();
+// import { anySample, noExistSample, primitiveSample, unknownSample } from "./basic";
+// anySample()
+// noExistSample()
+// primitiveSample()
+// unknownSample()
+
+
+//関数
+import { logMessage } from "./function/basic";
+import {isUserSignedIn} from "./function/parameter"
+
+logMessage("Hello TypeScript")
+
+isUserSignedIn('ABC', 'stamander')
